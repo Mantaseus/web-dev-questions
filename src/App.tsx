c1f375a41957a0ed0d49c9f4672c68d084a2d204
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-06-27 09:12:16 -0700

    define placeholder q48 and q49 and use reaect router for them
    - It should be relatively easy to define a new question to the app now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import './App.css'
+import * as q48 from './q48';
+import * as q49 from './q49';
+
+const PAGES = { q48, q49 };
+
+const PageList: React.FC = () => {
+  return <div className="page-list">
+    {Object.entries(PAGES).map(([pageName]) => <Link to={pageName}>{pageName}</Link>)}
+  </div>;
+};
 
 export function App() {
-  return (<div className="app">
-    Hello
-  </div>)
+  return (<BrowserRouter>
+    <Routes>
+      <Route path="/">
+        <Route index element={<PageList/>}/>
+        {Object.entries(PAGES).map(([pageName, { Question }]) => <Route path={pageName} element={<Question/>}/>)}
+      </Route>
+    </Routes>
+  </BrowserRouter>);
 };
---
+++ src/q48/index.tsx
@@ -0,0 +1,4 @@
+
+export const Question: React.FC = () => {
+  return <div>Some question</div>;
+};
