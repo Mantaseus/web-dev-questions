f47f2daa5e8981343d7d2e9e1bc2b1278dc98355
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-08 08:45:57 -0700

    feat: create a function that can test funcs in the console
    - Running the function it returns will make a `window.test()` function
      available in the browser console
    - We give it a list of arguments to test and an ideal solution function
      - The user can give it a function and run `test(theirFunction)` and
        `test()` will compare the outputs of their function to the outputs
        of the ideal function for each argument in the test args list
      - All of this in the browser console

---
+++ src/console_test.ts
@@ -0,0 +1,42 @@
+import equal from 'deep-equal';
+
+type AnyFunc = (...args: any[]) => any;
+
+export function createSetupTest<F extends AnyFunc>(testArgs: Parameters<F>[], idealSolution: F) {
+  return () => {
+    (window as any).test = async (func: F) => {
+      console.clear();
+      for (let i = 0; i < testArgs.length; i++) {
+        const args = testArgs[i];
+        const expected = idealSolution(...args);
+
+        const displayObj = [
+          '\nArgs:', ...args.flatMap(arg => ['\n  ', arg]),
+          '\nExpected result:', expected,
+        ]
+
+        try {
+          const result = await func(...args);
+          if (equal(result, expected)) {
+            console.log(
+              `%cTest ${i + 1}/${testArgs.length}: Success`, 'color: #89ffa6',
+              ...displayObj,
+              '\nYour result:', result,
+            )
+          } else {
+            console.error(`Test ${i + 1}/${testArgs.length}: Your result did not match expected result`,
+              ...displayObj,
+              '\nYour result:', result,
+            )
+          }
+        } catch(e) {
+          console.error(`Test ${i + 1}/${testArgs.length}: Error occured`,
+            ...displayObj,
+            '\nError from your function:', e,
+          )
+        }
+      }
+    }
+  }
+}
+
