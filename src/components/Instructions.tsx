e056f5a516080604c7e34b8d8ce077bcc0b49bfd
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-09 07:06:57 -0700

    feat: show instructions in a accordion (`<details/>`) element

--- src/components/Instructions.tsx
+++ src/components/Instructions.tsx
@@ -25,14 +25,21 @@ function mySolution(${args.map(arg => arg.name).join(', ')}) {
 test(mySolution)
   `.trim();
 
-  return (<>
-    <h4>Instructions</h4>
-    <ol>
-      <li>Open your browser console and run the code below</li>
-      <li>Tweak the code in the console and run it until all the tests succeed</li>
-    </ol>
-    <pre>
-      {code}
-    </pre>
-  </>);
+  return (<div className="instructions-container">
+    <details>
+      <summary><strong>Instructions on writing the code</strong></summary>
+      <ol>
+        <li>Open your browser console and run the code below</li>
+        <li>Copy the following code into the console</li>
+        <pre style={{ marginLeft: '1rem' }}>
+          {code}
+        </pre>
+        <li>Run it</li>
+        <ul>
+          <li>You will notice that it will run some tests and show you info about those tests</li>
+        </ul>
+        <li>Tweak the code in the console and run it until all the tests succeed</li>
+      </ol>
+    </details>
+  </div>);
 }
