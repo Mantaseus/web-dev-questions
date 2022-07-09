5989fabc917474aef96362fbf2f9936f0c37283a
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-09 09:59:50 -0700

    feat: show question completion status in header

--- src/components/Badges.tsx
+++ src/components/Badges.tsx
@@ -1,11 +1,16 @@
+export interface BadgeData {
+  text: string;
+  style?: React.HTMLAttributes<HTMLDivElement>['style'];
+}
+
 export interface Props {
-  badges: string[];
+  badges: BadgeData[];
 }
 
 export const Badges: React.FC<Props> = ({ badges }) => {
   return (<div className="badge-list">
-    {badges.map(badge => <div key={badge} className="badge">
-      {badge}
+    {badges.map(badge => <div key={badge.text} className="badge" style={badge.style}>
+      {badge.text}
     </div>)}
   </div>);
 };
--- src/components/QuestionHeader.tsx
+++ src/components/QuestionHeader.tsx
@@ -1,5 +1,5 @@
 import { getQuestionData } from "../console_test";
-import { Badges } from "./Badges";
+import { BadgeData, Badges } from "./Badges";
 import { useQuestionData, useQuestionKey } from "./hooks";
 
 export interface Props {
@@ -9,12 +9,27 @@ export interface Props {
 
 export const QuestionHeader: React.FC<Props> = ({ title, badges }) => {
   const questionData = useQuestionData();
+  const fullBadges = (badges || [])
+    .map<BadgeData>(text => ({ text }))
+    .concat(questionData.isCompleted
+      ? { text: 'Completed', style: { backgroundColor: 'green' } }
+      : questionData.lastAttemptTime
+        ? { text: 'Attempted', style: { backgroundColor: '#ddd', color: 'black' } }
+        : { text: 'Not attempted', style: { backgroundColor: '#ddd', color: 'black' } }
+    )
+
   return <>
     <h2>{questionData.key}. {title}</h2>
-    {questionData.lastAttemptTime && <small className="muted" style={{ display: 'inline-block', marginBottom: '1rem' }}>
-      Last attempted: {new Date(questionData.lastAttemptTime).toLocaleString()}
-    </small>}
 
-    {badges && <Badges badges={badges}/>}
+    <Badges badges={fullBadges}/>
+
+    <div className="question-attempt-info">
+      {questionData.lastAttemptTime && <small>
+        Last attempted: {new Date(questionData.lastAttemptTime).toLocaleString()}
+      </small>}
+      {questionData.completedTime && <small>
+        Completed: {new Date(questionData.completedTime).toLocaleString()}
+      </small>}
+    </div>
   </>
 }
