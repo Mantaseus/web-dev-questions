c3dd06c0af22c078721b9f2e9fd9c774600492fa
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-09 07:54:23 -0700

    feat: format ideal solution more nicely
    - The `func.toString()` represenation of my ideal function doesn't
      necessarily always look good by itself
      - For example: chained functions show up in a single line rather than
        maintaining their indentation
    - Installed `js-beautify` to handle that for us

--- src/components/IdealSolution.tsx
+++ src/components/IdealSolution.tsx
@@ -1,13 +1,33 @@
+import beautify, { JSBeautifyOptions } from 'js-beautify';
+
 export interface Props {
   func: Function;
 }
+
+const options: JSBeautifyOptions = {
+  indent_size: 4,
+  indent_char: " ",
+  max_preserve_newlines: 5,
+  preserve_newlines: true,
+  keep_array_indentation: false,
+  break_chained_methods: true,
+  brace_style: "collapse",
+  space_before_conditional: true,
+  unescape_strings: false,
+  jslint_happy: false,
+  end_with_newline: false,
+  wrap_line_length: 0,
+  comma_first: false,
+  e4x: false,
+  indent_empty_lines: false,
+};
 
 export const IdealSolution: React.FC<Props> = ({ func }) =>
   (<div className="ideal-solution-container">
     <details>
       <summary><strong>Ideal Solution</strong></summary>
       <pre>
-        {func.toString()}
+        {beautify.js(func.toString(), options)}
       </pre>
     </details>
   </div>);
