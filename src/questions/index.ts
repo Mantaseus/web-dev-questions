00c1e8fc15f6eedb67baba79b9ba0b6f9f24473e
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-10 14:57:57 -0700

    add question q5

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -2,5 +2,8 @@ import * as js1 from './js1';
 import * as js2 from './js2';
 import * as js3 from './js3';
 import * as js4 from './js4';
+import * as js5 from './js5';
 
-export const QUESTIONS = { js1, js2, js3, js4 };
+export const QUESTIONS = {
+  js1, js2, js3, js4, js5,
+};
---
+++ src/questions/js5.tsx
@@ -0,0 +1,33 @@
+import { QuestionWrapper, Props as QuestionWrapperProps } from "../components/QuestionWrapper";
+
+export const title = 'Uppercase every string in an array';
+export const badges = ['JS', 'Array.map', 'String.toUpperCase'];
+
+const questionWrapperProps: QuestionWrapperProps<(strArray: string[]) => string[]> = {
+  title,
+  badges,
+  funcArgsDoc: [
+    { name: 'str', type: 'string' },
+  ],
+  funcReturnDoc: { type: 'string[]' },
+  testArgs: [
+    [['make', 'ME', 'upper case']],
+    [['']],
+  ],
+  idealSolution: function mySolution(strArray) {
+    return strArray.map(str => str.toUpperCase());
+  }
+}
+
+export const Question: React.FC = () => <QuestionWrapper {...questionWrapperProps}>
+  <p>
+    Write a function that takes an array of strings and returns a new array of strings where all
+    of the original strings have been converted to uppercase
+  </p>
+
+  <h4>Examples</h4>
+  <ol>
+    <li>Calling your function with <code>mySolution('a', 'BC', 'de Fg')</code> should
+    return <code>['A', 'BC', 'DE F G']</code></li>
+  </ol>
+</QuestionWrapper>;
