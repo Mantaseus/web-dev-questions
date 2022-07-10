12b4348e42dc24d43a3f16d8271c7135219439ba
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-10 16:11:25 -0700

    add question js7

--- src/questions/index.ts
+++ src/questions/index.ts
@@ -4,7 +4,8 @@ import * as js3 from './js3';
 import * as js4 from './js4';
 import * as js5 from './js5';
 import * as js6 from './js6';
+import * as js7 from './js7';
 
 export const QUESTIONS = {
-  js1, js2, js3, js4, js5, js6,
+  js1, js2, js3, js4, js5, js6, js7,
 };
---
+++ src/questions/js7.tsx
@@ -0,0 +1,33 @@
+import { QuestionWrapper, Props as QuestionWrapperProps } from "../components/QuestionWrapper";
+
+export const title = 'Double numbers in an array';
+export const badges = ['JS', 'Array.map'];
+
+const questionWrapperProps: QuestionWrapperProps<
+  (numArr: number[]) => number[]
+> = {
+  funcTsTypeStr: `
+  (numArr: number[]) => number[]
+  `.trim(),
+  title,
+  badges,
+  testArgs: [
+    [[0, 1, 2, 10]],
+    [[]],
+  ],
+  idealSolution: function mySolution(numArr) {
+    return numArr.map(num => num * 2)
+  }
+}
+
+export const Question: React.FC = () => <QuestionWrapper {...questionWrapperProps}>
+  <p>
+    Write a function that takes an array of numbers, multiplies each number in the array by 2, and returns
+    these new numbers in an array
+  </p>
+
+  <h4>Examples</h4>
+  <ol>
+    <li>Calling your function with <code>mySolution([0, 1, 2, 3])</code> should return <code>[0, 2, 4, 6]</code></li>
+  </ol>
+</QuestionWrapper>;
