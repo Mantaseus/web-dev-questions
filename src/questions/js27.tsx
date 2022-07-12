c45cc16c275cf8752cc78d4a829121977ffb0ab7
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-12 07:41:25 -0700

    add js27

---
+++ src/questions/js27.tsx
@@ -0,0 +1,74 @@
+import { Link } from "react-router-dom";
+import { QuestionWrapper, Props as QuestionWrapperProps } from "../components/QuestionWrapper";
+
+export const title = 'Running average of an array';
+export const badges = ['JS', 'Array.map', 'Array.slice', 'Array.reduce'];
+
+const questionWrapperProps: QuestionWrapperProps<
+  (numArr: number[], avgLength: number) => number[]
+> = {
+  funcTsTypeStr: `
+  (numArr: number[], avgLength: number) => number[]
+  `.trim(),
+  title,
+  badges,
+  testArgs: [
+    [[1, 2, 3, 10, 15, 5], 3],
+    [[1, 2, 3, 100, 98, 106, 125, 88, 89, 93, 95, 92], 5],
+    [[-1, -2, -3, 10, 26, 52, 64, 100, 77, 89], 5],
+    [[], 5],
+  ],
+  idealSolution: function mySolution(numArr, avgLength) {
+    const indexOffset = avgLength - 1;
+    return numArr.map((num, i, arr) => {
+      const startIndex = Math.max(0, i - indexOffset);
+      const endIndex = i + 1;
+      const slice = arr.slice(startIndex, endIndex);
+      const sum = slice.reduce((sum, num) => sum + num);
+      return sum / slice.length;
+    });
+  }
+}
+
+export const Question: React.FC = () => <QuestionWrapper {...questionWrapperProps}>
+  <p>
+    This question builds on top of <Link to="/js26">js26</Link>. Your function in this question should take the average
+    of every slice (which you practiced in <Link to="/js24">js24</Link>) and return the average value in the returned array.
+  </p>
+  <p>
+    <strong>Aside</strong>: You have essentially calculated the <a href="https://en.wikipedia.org/wiki/Moving_average">Running average</a> of
+    the array which is a simple and effective way to implement a low-pass filter on a set of data. It essentially makes your data "smoother"
+    and can be useful in many applications especially when working with sensors. If you make <code>avgLength</code> higher then your data
+    will become even smoother.
+  </p>
+
+  <h4>Examples</h4>
+  <ol>
+    <li>
+      <code>mySolution([1, 2, 3, 10, 15, 5], 3)</code> should return
+      <pre>{`
+[
+  1,
+  1.5,
+  2,
+  5,
+  9.333333333333334,
+  10
+]
+      `.trim()}</pre>
+    </li>
+    <li><code>mySolution([])</code> should return <code>[]</code></li>
+  </ol>
+
+  <details className="hint">
+    <summary><strong>Hint</strong></summary>
+    <p>
+      The starting position of the slice should be <code>0</code> if <code>(i - (maxLength - 1)) &lt; 0</code> (where <code>i</code>
+      is the index of the array within the returned array). Otherwise it should be <code>(i - (maxLength - 1))</code>.
+    </p>
+    <p>
+      The ending position of the slice is <code>i + 1</code> (where <code>i</code> is the index of the
+      array within the returned array).
+    </p>
+  </details>
+</QuestionWrapper>;
