c772f919c6c5ba9d101d956d666f05172b0a696f
Satnam Singh Brar <brar.satnam1995@gmail.com>
2022-07-12 08:34:51 -0700

    add js31

--- src/questions/js31.tsx
+++ src/questions/js31.tsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
 import { QuestionWrapper, Props as QuestionWrapperProps } from "../components/QuestionWrapper";
 
-export const title = 'Count characters in a string and order them';
-export const badges = ['JS', 'String.split', 'Array.reduce', 'Object', 'Object.keys', 'Array.map', 'Array.sort'];
+export const title = 'Most frequent characters in a string';
+export const badges = ['JS', 'String.split', 'Array.reduce', 'Object', 'Object.keys', 'Array.map', 'Array.sort', 'Array.slice'];
 
 const questionWrapperProps: QuestionWrapperProps<
-  (str: string) => { char: string, count: number }[]
+  (str: string, topCount: number) => { char: string, count: number }[]
 > = {
   funcTsTypeStr: `
   (str: string) => { char: string, count: number }[]
@@ -13,12 +13,12 @@ const questionWrapperProps: QuestionWrapperProps<
   title,
   badges,
   testArgs: [
-    ['obfuscation'],
-    ['gesundheit'],
-    ['a test with multiple spaces and making a full sentence'],
-    [''],
+    ['obfuscation', 5],
+    ['gesundheit', 5],
+    ['a test with multiple spaces and making a full sentence', 5],
+    ['', 5],
   ],
-  idealSolution: function mySolution(str) {
+  idealSolution: function mySolution(str, topCount) {
     const countByChar = str.split('')
       .reduce((obj, char) => {
         obj[char] = (obj[char] || 0) + 1;
@@ -32,30 +32,31 @@ const questionWrapperProps: QuestionWrapperProps<
           count: countByChar[char],
         }
       })
-      .sort((a, b) => b.count - a.count);
+      .sort((a, b) => b.count - a.count)
+      .slice(0, topCount);
   }
 }
 
 export const Question: React.FC = () => <QuestionWrapper {...questionWrapperProps}>
-  <p>This question builds on top of <Link to="/js29">js29</Link>. Use your code from that question as your starting point.</p>
+  <p>This question builds on top of <Link to="/js30">js30</Link>. Use your code from that question as your starting point.</p>
   <p>
     This question assumes that your function can already take a string and create an array of objects with <code>char</code> and
-    <code>count</code> keys in each object.
+    <code>count</code> keys in each object. It is also assumed that the array of objects is sorted by <code>count</code> in descending
+    order.
   </p>
   <p>
-    Your function should now sort your array of objects by the <code>count</code> value in a desceding order.
+    Your function should now be able to take a second argument, <code>topCount</code>, which will be a number telling your function
+    to return only the top <code>topCount</code> number of elements from the array
   </p>
 
   <h4>Examples</h4>
   <ol>
     <li>
-      <code>mySolution('abbcccdddd')</code> should return
+      <code>mySolution('abbcccdddd', 2)</code> should return
       <pre>{`
 [
   { char: 'd', count: 4 },
   { char: 'c', count: 3 },
-  { char: 'b', count: 2 },
-  { char: 'a', count: 1 },
 ]
       `.trim()}</pre>
     </li>
